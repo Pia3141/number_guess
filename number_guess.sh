2e920c90db21b8ea281189640125abb9b1885f30
Allie Code <olympia.s314@gmail.com>
2024-02-18 11:33:55 +0000

    feat: add guess function

--- number_guess.sh
+++ number_guess.sh
@@ -3,10 +3,36 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 RANDOM_NUMBER=$((1 + RANDOM % 1000))
-
+TRIES=0
 
 ADD_USER(){
   INSERT_USER_RESULT="$($PSQL "INSERT INTO users(name) VALUES ('$USERNAME')")"
+}
+
+GUESS(){
+
+  if [[ -n $1 ]]
+  then echo -e "\n$1"
+  fi
+  
+  read GUESS_NUMBER
+  if [[ ! $GUESS_NUMBER =~ [0-9]+ ]]
+  then
+    GUESS "That is not an integer, guess again:"
+  fi
+
+  if [[ $GUESS_NUMBER -lt $RANDOM_NUMBER ]]
+  then
+    TRIES=$((TRIES+1))
+    GUESS "It's higher than that, guess again:"
+  elif [[ $GUESS_NUMBER -gt $RANDOM_NUMBER ]]
+  then
+    TRIES=$((TRIES+1))
+    GUESS "It's lower than that, guess again:"
+  else 
+   echo -e "\nYou guessed it in $TRIES tries. The secret number was $RANDOM_NUMBER. Nice job!"
+  fi
+  
 }
 
 GAME(){
@@ -24,6 +50,9 @@ GET_USER_ID=$($PSQL "SELECT user_id FROM users WHERE name='$USERNAME'")
     echo "Welcome back, $USERNAME! You have played games, and your best game took guesses."
   fi
 
+  echo -e "\nGuess the secret number between 1 and 1000:"
+  GUESS
+ 
 
 }
 
